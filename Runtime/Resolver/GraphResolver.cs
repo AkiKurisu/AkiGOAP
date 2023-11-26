fd0bc64742824457249da296ca7166d65370bad3
AkiKurisu <2683987717@qq.com>
2023-11-26 23:18:45 +0800

    Revert "Revert "Update for 2022""
    This reverts commit ae1d5ccb3d83c6102f1a4523853840455eb8b392.

--- Runtime/Resolver/GraphResolver.cs
+++ Runtime/Resolver/GraphResolver.cs
@@ -6,93 +6,106 @@ using Unity.Collections;
 /// </summary>
 namespace Kurisu.GOAP.Resolver
 {
-    public class GraphResolver:IGraphResolver
+    public class GraphResolver : IGraphResolver
     {
         private readonly List<Node> indexList;
         private readonly List<INode> nodeIndexList;
 
         private readonly List<NodeCondition> conditionList;
         private readonly List<GOAPState> conditionIndexList;
+#if UNITY_COLLECTIONS_1_3
         // Dictionary<ActionIndex, ConditionIndex[]>
-        private NativeMultiHashMap<int, int> nodeConditions;
+        private NativeParallelMultiHashMap<int, int> nodeConditions;
         // Dictionary<ConditionIndex, NodeIndex[]>
+        private NativeParallelMultiHashMap<int, int> conditionConnections;
+#else
+        private NativeMultiHashMap<int, int> nodeConditions;
         private NativeMultiHashMap<int, int> conditionConnections;
-        private Graph graph;
+#endif
+        private readonly Graph graph;
         public GraphResolver(IEnumerable<INode> nodes)
         {
-            this.graph = new GraphBuilder().Build(nodes);
-            this.indexList = this.graph.AllNodes.ToList();
-            this.nodeIndexList = this.indexList.Select(x => x.InternalNode).ToList();
-            
-            this.conditionList = this.indexList.SelectMany(x => x.Conditions).ToList();
-            this.conditionIndexList = this.conditionList.Select(x => x.Condition).ToList();
-            
-            this.CreateNodeConditions();
-            this.CreateConditionConnections();
+            graph = new GraphBuilder().Build(nodes);
+            indexList = graph.AllNodes.ToList();
+            nodeIndexList = indexList.Select(x => x.InternalNode).ToList();
+
+            conditionList = indexList.SelectMany(x => x.Conditions).ToList();
+            conditionIndexList = conditionList.Select(x => x.Condition).ToList();
+
+            CreateNodeConditions();
+            CreateConditionConnections();
         }
-        public int GetIndex(INode node) => this.nodeIndexList.IndexOf(node);
-        public INode GetNode(int index) => this.nodeIndexList[index];
+        public int GetIndex(INode node) => nodeIndexList.IndexOf(node);
+        public INode GetNode(int index) => nodeIndexList[index];
         private void CreateNodeConditions()
         {
-            var map = new NativeMultiHashMap<int, int>(this.indexList.Count, Allocator.Persistent);            
-            for (var i = 0; i < this.indexList.Count; i++)
+#if UNITY_COLLECTIONS_1_3
+            var map = new NativeParallelMultiHashMap<int, int>(indexList.Count, Allocator.Persistent);
+#else
+            var map = new NativeMultiHashMap<int, int>(indexList.Count, Allocator.Persistent);
+#endif
+            for (var i = 0; i < indexList.Count; i++)
             {
-                var conditions = this.indexList[i].Conditions
-                    .Select(x => this.conditionIndexList.IndexOf(x.Condition));
+                var conditions = indexList[i].Conditions
+                    .Select(x => conditionIndexList.IndexOf(x.Condition));
 
                 foreach (var condition in conditions)
                 {
                     map.Add(i, condition);
                 }
             }
-            
-            this.nodeConditions = map;
+
+            nodeConditions = map;
         }
 
         private void CreateConditionConnections()
         {
-            var map = new NativeMultiHashMap<int, int>(this.conditionIndexList.Count, Allocator.Persistent);
-            for (var i = 0; i < this.conditionIndexList.Count; i++)
+#if UNITY_COLLECTIONS_1_3
+            var map = new NativeParallelMultiHashMap<int, int>(conditionIndexList.Count, Allocator.Persistent);
+#else
+            var map = new NativeMultiHashMap<int, int>(conditionIndexList.Count, Allocator.Persistent);
+#endif
+            for (var i = 0; i < conditionIndexList.Count; i++)
             {
-                var connections = this.conditionList[i].Connections
-                    .Select(x => this.indexList.IndexOf(x));
+                var connections = conditionList[i].Connections
+                    .Select(x => indexList.IndexOf(x));
 
                 foreach (var connection in connections)
                 {
                     map.Add(i, connection);
                 }
             }
-            
-            this.conditionConnections = map;
+
+            conditionConnections = map;
         }
 
         public IResolveHandle StartResolve(RunData runData)
         {
-            return new ResolveHandle(this, this.nodeConditions, this.conditionConnections, runData);
+            return new ResolveHandle(this, nodeConditions, conditionConnections, runData);
         }
         public IExecutableBuilder GetExecutableBuilder()
         {
-            return new ExecutableBuilder(this.nodeIndexList);
+            return new ExecutableBuilder(nodeIndexList);
         }
-        
+
         public IPositionBuilder GetPositionBuilder()
         {
-            return new PositionBuilder(this.nodeIndexList);
+            return new PositionBuilder(nodeIndexList);
         }
 
         public ICostBuilder GetCostBuilder()
         {
-            return new CostBuilder(this.nodeIndexList);
+            return new CostBuilder(nodeIndexList);
         }
-        
+
         public IConditionBuilder GetConditionBuilder()
         {
-            return new ConditionBuilder(this.conditionIndexList);
+            return new ConditionBuilder(conditionIndexList);
         }
         public void Dispose()
         {
-            this.nodeConditions.Dispose();
-            this.conditionConnections.Dispose();
+            nodeConditions.Dispose();
+            conditionConnections.Dispose();
         }
     }
 }
